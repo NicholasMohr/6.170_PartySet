c163f9f7d610af5291882b164ef293b4711d64eb
Nick Mohr <nmohr@mit.edu>
2014-11-17 22:29:32 -0500

    oopsies much typo

--- routes/parties.js
+++ routes/parties.js
@@ -13,24 +13,24 @@ router.post('/', function (req, res) {
 });
 
 router.get('/', function (req, res) {
-    controller.getParties(req, res) {
+    controller.getParties(req, res);
 });
 
 //get all the party info
 router.get('/:id', function (req, res) {
-    controller.getPartyInfo(req, res) {
+    controller.getPartyInfo(req, res);
 });
 
 //TODO: test this
 //add current user to party
 router.put('/:id', function (req, res) {
-    controller.addToParty(req, res) {
+    controller.addToParty(req, res);
 });
 
 //TODO: test this
 //remove current user from party
 router.delete('/:id', function (req, res) {
-    controller.removeFromParty(req, res) {    
+    controller.removeFromParty(req, res);    
 });
 
 module.exports = router;
