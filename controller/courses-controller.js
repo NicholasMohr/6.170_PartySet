8dd333a99b2dd654c00a66d173faa2adba19e4ec
hyun94 <hyun94@mit.edu>
2014-11-18 20:46:24 -0500

    fixed check for deadline

--- controller/courses-controller.js
+++ controller/courses-controller.js
@@ -27,8 +27,7 @@ var controller = function(){
             get all parties in a course
         */
         getPartiesOfCourse: function(req, res) {
-            //TODO: delete all the things that are before a certain date.
-            Parties.activeForCourse({course : req.params.course_id}, function(err,parties){
+            Parties.activeForCourse(req.params.course_id, function(err, parties) {
                 if (err) {
                     utils.sendErrResponse(res, 500, 'An unexpected error occured.');
                 } else {
