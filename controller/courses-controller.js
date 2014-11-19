5078a10dd0609b16b37562b2fa6831f24701d6f2
hyun94 <hyun94@mit.edu>
2014-11-18 19:52:01 -0500

    working on courses-controller

--- controller/courses-controller.js
+++ controller/courses-controller.js
@@ -27,12 +27,12 @@ var controller = function(){
             get all parties in a course
         */
         getPartiesOfCourse: function(req, res) {
-            //TODO: delete all the things that are before a certain date.
-            Parties.find({course : req.params.course_id}, function(err,parties){
+            // TODO: delete all the things that are before a certain date.
+            Parties.find({course: req.params.course_id}, function(err,parties){
                 if (err) {
                     utils.sendErrResponse(res, 500, 'An unexpected error occured.');
                 } else {
-                    res.json(parties)
+                    res.json(parties);
                 }
             });
         },
