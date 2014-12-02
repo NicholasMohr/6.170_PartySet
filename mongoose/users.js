23265f9e8238c9e36ea4a85d9664cb63694750e4
hyun94 <hyun94@mit.edu>
2014-12-02 18:14:55 -0500

    required attributes in models to be true

--- mongoose/users.js
+++ mongoose/users.js
@@ -5,11 +5,18 @@ var Schema = mongoose.Schema;
 
 var userSchema = Schema({
     name: {type: String, required: true},
-    password: {type: String, required: true},
+    password: {type: String, required: true, },
     email: {type: String, required: true, unique: true, lowercase:true},
     verified: {type: Boolean, required: true}, // for future implementation: email verified or not
     party: {type: Schema.Types.ObjectId, ref: 'Party'},
     courses: [{type: Schema.Types.ObjectId, ref: 'Course'}]
 });
+
+var User = mongoose.model('User', userSchema);
+
+User.schema.path('password').validate(function (value) {
+  return value.length > 7;
+}, 'Your password should be at least 7 characters long.');
+
 
 module.exports = mongoose.model('User', userSchema);
